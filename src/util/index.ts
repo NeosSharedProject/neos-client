7b88c289babd058276c31e23f770446071ff0b55
rhenium <rhenium750@gmail.com>
2022-11-06 16:58:02 +0900

    feat: add neosdb2https

--- src/util/index.ts
+++ src/util/index.ts
@@ -4,3 +4,19 @@ export function escapeTagString(str: string) {
     ""
   );
 }
+
+export function neosdb2https(neosdbUrl: string): string | undefined {
+  if (!neosdbUrl) {
+    return undefined;
+  }
+  const sl0 = neosdbUrl?.split("///");
+  if (sl0.length != 2) {
+    return undefined;
+  }
+  const sl1 = sl0[1].split(".");
+  if (sl1.length != 2) {
+    return `https://cloudxstorage.blob.core.windows.net/assets/${sl0[1]}`;
+  } else {
+    return `https://cloudxstorage.blob.core.windows.net/assets/${sl1[0]}`;
+  }
+}
