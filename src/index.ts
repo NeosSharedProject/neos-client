644645af290344acd4c92b47b4b36c4af1e19c5f
rhenium <rhenium750@gmail.com>
2022-11-15 00:19:03 +0900

    feat: export

---
+++ src/index.ts
@@ -0,0 +1,173 @@
+import { getFriends, addFriend } from "./api/friends";
+import {
+  negotiateHub,
+  connectHub,
+  sendMessage as sendMessageHub,
+  markMessageRead as marMessageReadHub,
+} from "./api/hub";
+import {
+  getMessages,
+  sendTextMessage,
+  markMessageRead,
+  sendKFC,
+} from "./api/messages";
+import { getUser, getUserStatus, putUserStatus } from "./api/users";
+import { deleteUserSession, postUserSession } from "./api/userSessions";
+import { Neos as NeosClient } from "./client";
+import {
+  NeosDateStringType,
+  NeosSessionUrlType,
+  NeosUriType,
+  NeosVersionType,
+} from "./type/common";
+import { NeosFriendType } from "./type/friend";
+import {
+  EventCallbackListType,
+  MessagesReadEventArgumentType,
+  ReceiveMessageEventArgumentType,
+} from "./type/hub";
+import {
+  NeosGroupIdType,
+  NeosIdType,
+  NeosMachineIdType,
+  NeosMessageIdType,
+  NeosOwnerIdType,
+  NeosRecordIdType,
+  NeosSessionIdType,
+  NeosUserIdType,
+} from "./type/id";
+import {
+  CreditTransferMessageContentType,
+  CreditTransferMessageType,
+  MessageType,
+  NeosCreditTransferMessageType,
+  NeosMessageType,
+  NeosObjectMessageType,
+  NeosSessionInviteMessageType,
+  NeosSoundMessageType,
+  NeosTextMessageType,
+  ObjectMessageContentType,
+  ObjectMessageType,
+  SessionInviteMessageContentType,
+  SessionInviteMessageType,
+  SoundMessageContentType,
+  SoundMessageType,
+  TextMessageContentType,
+  TextMessageType,
+} from "./type/message";
+import { NeosSessionType, NeosSessionUserType } from "./type/session";
+import { NeosUserStatusType, NeosUserType } from "./type/user";
+import {
+  NeosLoginCredentialType,
+  NeosUserSessionType,
+} from "./type/userSession";
+import { escapeTagString, neosdb2https } from "./util";
+import {
+  generateTextMessage,
+  getCreditTransferMessageContent,
+  getObjectMessageContent,
+  getSessionInviteMessageContent,
+  getSoundMessageContent,
+  parseNeosMessage,
+} from "./util/message";
+import { getAuthHeader, isPasswordCredential } from "./util/userSession";
+
+export const Neos = NeosClient;
+export type Neos = NeosClient;
+
+export const API = {
+  getFriends,
+  addFriend,
+  getMessages,
+  sendTextMessage,
+  markMessageRead,
+  sendKFC,
+  getUser,
+  getUserStatus,
+  putUserStatus,
+  postUserSession,
+  deleteUserSession,
+};
+
+export const Hub = {
+  negotiateHub,
+  connectHub,
+  sendMessage: sendMessageHub,
+  markMessage: marMessageReadHub,
+};
+
+export namespace Util {
+  export const Common = {
+    escapeTagString,
+    neosdb2https,
+  };
+  export const Message = {
+    getObjectMessageContent,
+    getCreditTransferMessageContent,
+    getSessionInviteMessageContent,
+    getSoundMessageContent,
+    parseNeosMessage,
+    generateTextMessage,
+  };
+  export const UserSession = {
+    getAuthHeader,
+    isPasswordCredential,
+  };
+}
+
+export namespace Type {
+  //common
+  export type NeosDateString = NeosDateStringType;
+  export type NeosVersion = NeosVersionType;
+  export type NeosUri = NeosUriType;
+  export type NeosSessionUrl = NeosSessionUrlType;
+
+  //friend
+  export type NeosFriend = NeosFriendType;
+
+  //hub
+  export type ReceiveMessageEventArgument = ReceiveMessageEventArgumentType;
+  export type MessagesReadEventArgument = MessagesReadEventArgumentType;
+  export type EventCallbackList = EventCallbackListType;
+
+  //id
+  export type NeosId<T extends string> = NeosIdType<T>;
+  export type NeosUserId = NeosUserIdType;
+  export type NeosGroupId = NeosGroupIdType;
+  export type NeosOwnerId = NeosOwnerIdType;
+  export type NeosRecordId = NeosRecordIdType;
+  export type NeosMessageId = NeosMessageIdType;
+  export type NeosSessionId = NeosSessionIdType;
+  export type NeosMachineId = NeosMachineIdType;
+
+  //message
+  export type NeosTextMessage = NeosTextMessageType;
+  export type NeosObjectMessage = NeosObjectMessageType;
+  export type NeosCreditTransferMessage = NeosCreditTransferMessageType;
+  export type NeosSessionInviteMessage = NeosSessionInviteMessageType;
+  export type NeosSoundMessage = NeosSoundMessageType;
+  export type NeosMessage = NeosMessageType;
+  export type TextMessageContent = TextMessageContentType;
+  export type ObjectMessageContent = ObjectMessageContentType;
+  export type CreditTransferMessageContent = CreditTransferMessageContentType;
+  export type SessionInviteMessageContent = SessionInviteMessageContentType;
+  export type SoundMessageContent = SoundMessageContentType;
+  export type TextMessage = TextMessageType;
+  export type ObjectMessage = ObjectMessageType;
+  export type CreditTransferMessage = CreditTransferMessageType;
+  export type SessionInviteMessage = SessionInviteMessageType;
+  export type SoundMessage = SoundMessageType;
+  export type Message = MessageType;
+
+  //session
+  export type NeosSessionUser = NeosSessionUserType;
+  export type NeosSession = NeosSessionType;
+
+  //user
+  export type NeosUserStatus = NeosUserStatusType;
+  export type NeosUser = NeosUserType;
+
+  //userSession
+  export type NeosLoginCredential = NeosLoginCredentialType;
+  export type NeosUserSession = NeosUserSessionType;
+}
